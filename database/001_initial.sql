5e40d2a21ba8097f4cf1c4d09cb00ae07e3446fa
aashumalik784 <aashumalik784@gmail.com>
2026-06-28 18:35:57 +0530

    001_initial.sql
    This file serves as the initial migration schema for the database.

---
+++ database/001_initial.sql
@@ -0,0 +1,6 @@
+-- Migration 001: Initial schema
+
+-- This file is the same as schema.sql
+-- In production, use a migration tool like Alembic or Flyway
+
+-- See schema.sql for the complete schema
